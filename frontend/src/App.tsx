94c66572d6064b7b657bcae4cb16ba5368cbc02b
eelfy <eeflyza@gmail.com>
2022-08-13 18:45:50 +0300

    add routes in app.tsx and etc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
-import "./App.css";
+import {
+  Route, Routes, BrowserRouter,
+} from 'react-router-dom';
+import Contacts from './pages/Contacts';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return <div />;
-}
+import './App.css';
+
+const App = () => (
+  <div className="app">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route path="login" element={<Login />} />
+          <Route path="contacts" element={<Contacts />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
